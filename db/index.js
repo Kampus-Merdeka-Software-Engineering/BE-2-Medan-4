f7db3cb177706a6cc0bdc96fd15a56deb393d83a
rendihardi <rendirenhard@gmail.com>
2023-12-01 16:19:18 +0700

    add env

--- db/index.js
+++ db/index.js
@@ -1,12 +1,19 @@
+require("dotenv").config();
+
 // db/index.js
 const { Sequelize } = require("sequelize");
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize("group_4", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-  port: 3306,
-});
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: process.env.DB_PORT,
+  }
+);
 
 const db = {};
 db.sequelize = sequelize;
